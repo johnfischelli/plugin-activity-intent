fba382047f2d2a45ba842ed19114013a7b3b5121
John Fischelli <john.fischelli@gmail.com>
2019-07-11 22:36:59 +0100

    initial commit

---
+++ src/ActivityIntentPlugin.js
@@ -0,0 +1,38 @@
+import { FlexPlugin } from 'flex-plugin';
+
+const PLUGIN_NAME = 'ActivityIntentPlugin';
+
+export default class ActivityIntentPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
+  init(flex, manager) {
+
+    flex.Actions.addListener('beforeSetActivity', (payload, abort) => {
+      let state = manager.store.getState();
+      if (state.flex.worker.tasks.size) {
+        window.localStorage.setItem('intendedActivity', payload.activitySid);
+        abort();
+      }
+    })
+
+    flex.Actions.addListener('afterCompleteTask', (payload) => {
+      setTimeout(() => {
+        let state = manager.store.getState();
+        let intended = window.localStorage.getItem('intendedActivity');
+        if (!state.flex.worker.tasks.size && intended) {
+          flex.Actions.invokeAction('SetActivity', {
+            activitySid: intended,
+            rejectPendingReservations: true
+          })
+          window.localStorage.removeItem('intendedActivity');
+        }
+      }, 500)
+    })
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import ActivityIntentPlugin from './ActivityIntentPlugin';
+
+FlexPlugin.loadPlugin(ActivityIntentPlugin);
